34f998ac609166bd0551dfd7d43a7ed22189ee87
Nikhil <97237226+DecoderNikhil@users.noreply.github.com>
2025-05-01 15:17:15 +0530

    Added hovering effect with both methods

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const conatiner = document.querySelector(".container");
 for (let i = 0; i < 16 * 16; i++) {
   conatiner.insertAdjacentHTML("beforeend", '<div class="box"></div>');
 }
+
+const box = document.querySelector(".box");
+conatiner.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("box")) {
+    // console.log(e);
+
+    // 1st method
+    // e.target.style.backgroundColor = "black";
+
+    // 2nd method
+    e.target.classList.add("blackBG");
+  }
+});
